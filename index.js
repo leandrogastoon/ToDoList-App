3176987e85b8cee3ddb6bd147b7136713e06f984
leandro martinez <llggmm23@gmail.com>
2023-03-04 16:53:22 -0300

    botones

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const lista = document.querySelector("ul");
 boton.addEventListener("click", agregar);
 
 
+
 function agregar(){
-    const tarea = input.value;
+    let tarea = input.value;
 
     const li = document.createElement("li");  //creo el elemento li y lo guardo en la variable
     li.textContent = tarea;  //agrego al li el contenido de la var tarea
     lista.appendChild(li); // agrego al ul el elemento li
     
+    const btnTareaTerminada = document.createElement("button");
+    btnTareaTerminada.textContent = "ok";
+    li.appendChild(btnTareaTerminada);
+
+    const btnBorrar = document.createElement("button");
+    btnBorrar.textContent = "delete";
+    li.appendChild(btnBorrar);
+    li.classList.add("itemNuevo");
+    
 }
+
+
