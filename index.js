cef06d8a23e92822aa97a78ed2a8edcd4c373468
leandro martinez <llggmm23@gmail.com>
2023-03-06 17:49:40 -0300

    .
    .

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const boton = document.querySelector(".boton");
 const input = document.querySelector(".inputText");
 const lista = document.querySelector("ul");
+const sinTareas = document.querySelector("h2");
 
-boton.addEventListener("click", agregar);
+boton.addEventListener("click", msj);
 
+
+function msj(){
+    if (input.value === "") {
+        alert("debe ingresar una tarea");
+    } else {
+        agregar();
+    }
+}
 
 
 function agregar(){
+    sinTareas.textContent = "";
+ 
     let tarea = input.value;
 
     const li = document.createElement("li");  //creo el elemento li y lo guardo en la variable
@@ -14,14 +25,31 @@ function agregar(){
     lista.appendChild(li); // agrego al ul el elemento li
     
     const btnTareaTerminada = document.createElement("button");
+    btnTareaTerminada.classList.add("tareaOk");
     btnTareaTerminada.textContent = "ok";
     li.appendChild(btnTareaTerminada);
 
+    btnTareaTerminada.addEventListener("click", (e) => {
+        li.removeChild(btnTareaTerminada);
+        li.removeChild(btnBorrar);
+        
+        const txtOk = document.createElement("H5");
+        txtOk.textContent = "Terminada";
+        li.appendChild(txtOk);
+        txtOk.classList.add("textoTareaOk");
+    });
+
     const btnBorrar = document.createElement("button");
+    btnBorrar.classList.add("borrar");
     btnBorrar.textContent = "delete";
     li.appendChild(btnBorrar);
     li.classList.add("itemNuevo");
     
+    btnBorrar.addEventListener("click", (e) => {  //evento del boton delete la tarea
+        lista.removeChild(li);
+    });
+
+    input.value = "";
 }
 
 
